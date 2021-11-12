bee0600831a84e35cbff2f049b37cf146eb7604d
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2021-11-12 21:56:51 +0300

    Added Android project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,11 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-    }
+plugins {
+    id("com.android.application") version "7.2.0-alpha04" apply false
+    id("com.android.library") version "7.2.0-alpha04" apply false
+    id("org.jetbrains.kotlin.android") version "1.5.31" apply false
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+    apply(from = "$rootDir/ktlint.gradle.kts")
 }
 
 tasks.register("clean").configure {
