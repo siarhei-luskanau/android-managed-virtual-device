85b8daff5e0c366fbc3ca7f276fddf57b0f8b9d2
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2021-11-12 22:15:46 +0300

    Added matrix by OS of to Github actions

--- app/src/test/java/siarhei/luskanau/managed/virtual/device/ExampleUnitTest.kt
+++ app/src/test/java/siarhei/luskanau/managed/virtual/device/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package siarhei.luskanau.managed.virtual.device
 
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,6 +9,7 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
