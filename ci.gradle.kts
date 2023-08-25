cb24414ce38c1a01da5da87f1b0a615d0ab02f47
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2023-08-25 02:27:25 +0200

    Updated Gradle wrapper and nested gradlew invocation in ci.gradle.kts

--- ci.gradle.kts
+++ ci.gradle.kts
@@ -1,4 +1,5 @@
 import org.apache.tools.ant.taskdefs.condition.Os
+import java.util.Properties
 
 val CI_GRADLE = "CI_GRADLE"
 
@@ -51,7 +52,7 @@ tasks.register("devEmulatorAll") {
 
 fun gradlew(
     vararg tasks: String,
-    addToEnvironment: Map<String, String>? = null,
+    addToSystemProperties: Map<String, String>? = null,
 ) {
     exec {
         executable = File(
@@ -60,12 +61,39 @@ fun gradlew(
         )
             .also { it.setExecutable(true) }
             .absolutePath
-        args = mutableListOf<String>().apply {
-            addAll(tasks)
-            add("--stacktrace")
+        args = mutableListOf<String>().also { mutableArgs ->
+            mutableArgs.addAll(tasks)
+            addToSystemProperties?.toList()?.map { "-D${it.first}=${it.second}" }?.let {
+                mutableArgs.addAll(it)
+            }
+            mutableArgs.add("--stacktrace")
         }
-        addToEnvironment?.let {
-            environment = environment.toMutableMap().apply { putAll(it) }
+        val sdkDirPath = Properties().apply {
+            val propertiesFile = File(rootDir, "local.properties")
+            if (propertiesFile.exists()) {
+                load(propertiesFile.inputStream())
+            }
+        }.getProperty("sdk.dir")
+        if (sdkDirPath != null) {
+            val platformToolsDir = "$sdkDirPath${java.io.File.separator}platform-tools"
+            val pahtEnvironment = System.getenv("PATH").orEmpty()
+            if (!pahtEnvironment.contains(platformToolsDir)) {
+                environment = environment.toMutableMap().apply {
+                    put("PATH", "$platformToolsDir:$pahtEnvironment")
+                }
+            }
+        }
+        if (System.getenv("JAVA_HOME") == null) {
+            System.getProperty("java.home")?.let { javaHome ->
+                environment = environment.toMutableMap().apply {
+                    put("JAVA_HOME", javaHome)
+                }
+            }
+        }
+        if (System.getenv("ANDROID_HOME") == null) {
+            environment = environment.toMutableMap().apply {
+                put("ANDROID_HOME", "$sdkDirPath")
+            }
         }
         println("commandLine: ${this.commandLine}")
     }.apply { println("ExecResult: $this") }
