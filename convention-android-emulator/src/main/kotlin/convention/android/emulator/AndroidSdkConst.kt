f61410e371b3842e9ca8e59d9db5df5ccbf07990
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-08-16 04:10:31 +0200

    Test: Run API 36 on ubuntu-24.04-arm

---
+++ convention-android-emulator/src/main/kotlin/convention/android/emulator/AndroidSdkConst.kt
@@ -0,0 +1,8 @@
+package convention.android.emulator
+
+internal object AndroidSdkConst {
+    const val PLATFORM = 36
+    const val BUILD_TOOLS_VERSION = "36.1.0"
+    const val CMDLINETOOLS_VERSION = "19.0"
+    const val COMMANDLINETOOLS_VERSION = "13114758"
+}
