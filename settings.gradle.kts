55f65fc8fec236539a45ea26dc96ace8988b63c6
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-08-16 04:10:31 +0200

    Test: Run API 37 on ubuntu-24.04-arm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,17 @@
+rootProject.name = "android-managed-virtual-device"
+include(
+    ":app"
+)
+
 pluginManagement {
+    includeBuild("convention-android-emulator")
     repositories {
         gradlePluginPortal()
         google()
         mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -12,7 +19,3 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
-rootProject.name = "android-managed-virtual-device"
-include(
-    ":app"
-)
