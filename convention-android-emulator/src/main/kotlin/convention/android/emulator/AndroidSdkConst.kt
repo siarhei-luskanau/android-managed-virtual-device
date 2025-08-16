2d8bb0ed5dc8eb78e9bda1ba1c17f7f103bdd783
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-08-16 04:10:31 +0200

    Test: Run API 37 on ubuntu-24.04-arm

---
+++ convention-android-emulator/src/main/kotlin/convention/android/emulator/AndroidSdkConst.kt
@@ -0,0 +1,8 @@
+package convention.android.emulator
+
+internal object AndroidSdkConst {
+    const val PLATFORM = "37.0"
+    const val BUILD_TOOLS_VERSION = "37.0.0"
+    const val CMDLINETOOLS_VERSION = "20.0"
+    const val COMMANDLINETOOLS_VERSION = "14742923"
+}
