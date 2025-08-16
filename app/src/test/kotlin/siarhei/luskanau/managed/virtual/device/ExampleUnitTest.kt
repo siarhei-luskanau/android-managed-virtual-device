c0505f2afec04fbea64f4cdcbe714e890e9e7e4a
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-08-16 04:10:31 +0200

    Test: Run API 37 on ubuntu-24.04-arm

--- app/src/test/kotlin/siarhei/luskanau/managed/virtual/device/ExampleUnitTest.kt
+++ app/src/test/kotlin/siarhei/luskanau/managed/virtual/device/ExampleUnitTest.kt
@@ -1,7 +1,7 @@
 package siarhei.luskanau.managed.virtual.device
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
